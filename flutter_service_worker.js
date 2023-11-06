1d8efa37e81da078030e09a9e545c50674c1a4b7
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-06 23:24:05 +0200

    Built web
    Branch: main
    Commit: 51d7ca390df94930e5e484e20906a4167621a48c
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,8 +48,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "4d3b358578105d2035962bbd7d0d671b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f514c29c914ede2034b51f36e8963900",
-"/": "f514c29c914ede2034b51f36e8963900",
+"index.html": "ef55679b6e8e88ed944516fd6ca0c10e",
+"/": "ef55679b6e8e88ed944516fd6ca0c10e",
 "main.dart.js": "a841890e947d252ede5b1b559ea8334f",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"};
